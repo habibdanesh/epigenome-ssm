ae2eba15a005d0c12d7a56574bcaac64dbae0036
Habib Daneshpajouh <habib.dpajouh@gmail.com>
2023-01-02 01:15:58 -0800

    split prepare_data by chromosome

--- workflow/scripts/generate_bin_regions.py
+++ workflow/scripts/generate_bin_regions.py
@@ -4,35 +4,46 @@ import os
 regions_file = snakemake.input.regions_file
 bin_size = snakemake.params.bin_size
 bin_regions_file = snakemake.output.bin_regions_file
+chrom_regions_files = snakemake.output.chrom_regions_files
 
 ### sort the input regions file to make sure chrom bin counter is accurate
 regions_file_sorted = '/'.join(bin_regions_file.split('/')[:-1]) + "/regions.sorted.bed.tmp"
 os.system("sort -k1V -k2n -k3n {} > {}".format(regions_file, regions_file_sorted))
 ###
 current_chrom = ''
-chrom_counter = 0
-bin_counter = 1
-with open(regions_file_sorted, 'r') as in_f, open(bin_regions_file, 'w') as out_f:
+region_counter = 0
+with open(regions_file_sorted, 'r') as in_f:
     in_regions = in_f.readlines()
-    for line in in_regions:
-        columns = line.split("\t")
-        chrom = columns[0]
-        if chrom != current_chrom:
-            current_chrom = chrom
-            chrom_counter += 1
-            bin_counter = 1
-        region_start = int(columns[1])
-        region_end = int(columns[2])
-        bin_start = region_start
-        bin_end = bin_start + bin_size
-        while bin_start < region_end:
-            if bin_end > region_end:
-                bin_end = region_end
-            bin_name = "{}_{}".format(chrom_counter, bin_counter)
-            print("{}\t{}\t{}\t{}".format(chrom, bin_start, bin_end, bin_name), file=out_f)
-            bin_counter += 1
-            # move
-            bin_start = bin_end
-            bin_end += bin_size
-###
+for chrom_counter, chrom_regions_file in enumerate(chrom_regions_files):
+    bin_counter = 1
+    with open(chrom_regions_file, 'w') as chrom_regions_f:
+        for line in in_regions[region_counter:]:
+            columns = line.split("\t")
+            chrom = columns[0]
+            if (chrom != current_chrom) and (current_chrom != ''): # not first chrom
+                current_chrom = chrom
+                break
+            elif chrom != current_chrom: # first chrom
+                current_chrom = chrom
+            region_counter += 1
+            region_start = int(columns[1])
+            region_end = int(columns[2])
+            bin_start = region_start
+            bin_end = bin_start + bin_size
+            while bin_start < region_end:
+                if bin_end > region_end:
+                    bin_end = region_end
+                bin_name = "{}{}".format(chrom_counter, bin_counter)
+                print("{}\t{}\t{}\t{}".format(chrom, bin_start, bin_end, bin_name), file=chrom_regions_f)
+                bin_counter += 1
+                # move
+                bin_start = bin_end
+                bin_end += bin_size
+### combine regions from multiple chroms into a single file
+cmd = "cat"
+for f in chrom_regions_files:
+    cmd += " " + f
+cmd += " > " + bin_regions_file
+os.system(cmd)
+### remove temp files
 os.remove(regions_file_sorted)
--- workflow/scripts/get_bigwig_subset.py
+++ workflow/scripts/get_bigwig_subset.py
@@ -2,23 +2,35 @@ import os
 
 ### snakemake parameters
 in_files = snakemake.input.in_files
-regions_file = snakemake.input.regions_file
+regions_files = snakemake.input.regions_files
 bw_avg_over_bed_src = snakemake.input.bw_avg_over_bed_src
 out_files = snakemake.output.out_files
 
 for in_f, out_f in zip(in_files, out_files):
-    out_tab_f = "{}.tab".format(out_f)
-    out_bed_f = "{}.tmp".format(out_f)
-    cmd = "{} {} {} {} -bedOut={}".format(
-            bw_avg_over_bed_src, in_f, regions_file, out_tab_f, out_bed_f)
-    print(cmd)
+    temp_out_files = []
+    for i, regions_file in enumerate(regions_files):
+        out_tab_f = "{}.tab.tmp".format(out_f)
+        out_bed_f = "{}.bed.tmp".format(out_f)
+        temp_out_f = "{}.{}.tmp".format(out_f, i)
+        temp_out_files.append(temp_out_f)
+        cmd = "{} {} {} {} -bedOut={}".format(
+                bw_avg_over_bed_src, in_f, regions_file, out_tab_f, out_bed_f)
+        os.system(cmd)
+        ### sort
+        cmd = "sort -k1V -k2n -k3n {} > {}".format(out_bed_f, temp_out_f)
+        os.system(cmd)
+        ### remove temp files
+        os.remove(out_tab_f)
+        os.remove(out_bed_f)
+    ### combine results from multiple out files
+    cmd = "cat"
+    for temp_out_f in temp_out_files:
+        cmd += " " + temp_out_f
+    cmd += " > " + out_f
     os.system(cmd)
-    print('')
-    ### sort
-    cmd = "sort -k1V -k2n -k3n {} > {}".format(out_bed_f, out_f)
-    print(cmd)
-    os.system(cmd)
-    print('')
     ### remove temp files
-    os.remove(out_tab_f)
-    os.remove(out_bed_f)
+    for temp_out_f in temp_out_files:
+        os.remove(temp_out_f)
+### remove temp regions files
+for regions_file in regions_files:
+    os.remove(regions_file)
