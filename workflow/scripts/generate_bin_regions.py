4c4982ec33d5d1486502d45ceac6d4e0a8a545ac
Habib Daneshpajouh <habib.dpajouh@gmail.com>
2023-01-01 21:29:28 -0800

    shorter chrom naming

--- workflow/scripts/generate_bin_regions.py
+++ workflow/scripts/generate_bin_regions.py
@@ -10,7 +10,8 @@ regions_file_sorted = '/'.join(bin_regions_file.split('/')[:-1]) + "/regions.sor
 os.system("sort -k1V -k2n -k3n {} > {}".format(regions_file, regions_file_sorted))
 ###
 current_chrom = ''
-bin_counter = 0
+chrom_counter = 0
+bin_counter = 1
 with open(regions_file_sorted, 'r') as in_f, open(bin_regions_file, 'w') as out_f:
     in_regions = in_f.readlines()
     for line in in_regions:
@@ -18,7 +19,8 @@ with open(regions_file_sorted, 'r') as in_f, open(bin_regions_file, 'w') as out_
         chrom = columns[0]
         if chrom != current_chrom:
             current_chrom = chrom
-            bin_counter = 0
+            chrom_counter += 1
+            bin_counter = 1
         region_start = int(columns[1])
         region_end = int(columns[2])
         bin_start = region_start
@@ -26,7 +28,7 @@ with open(regions_file_sorted, 'r') as in_f, open(bin_regions_file, 'w') as out_
         while bin_start < region_end:
             if bin_end > region_end:
                 bin_end = region_end
-            bin_name = "{}_{}".format(chrom, bin_counter)
+            bin_name = "{}_{}".format(chrom_counter, bin_counter)
             print("{}\t{}\t{}\t{}".format(chrom, bin_start, bin_end, bin_name), file=out_f)
             bin_counter += 1
             # move
