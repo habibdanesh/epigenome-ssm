74cb39bc022fe0212c5dfba96b453a898e0eed69
Habib Daneshpajouh <habib.dpajouh@gmail.com>
2023-01-18 11:51:49 -0800

    split and run annotation in multiple smaller chunks

--- workflow/scripts/generate_bin_regions.py
+++ workflow/scripts/generate_bin_regions.py
@@ -4,6 +4,7 @@ import os
 regions_file = snakemake.input.regions_file
 bin_size = snakemake.params.bin_size
 bin_regions_file = snakemake.output.bin_regions_file
+n_bins_file = snakemake.output.n_bins_file
 chrom_regions_files = snakemake.output.chrom_regions_files
 
 ### sort the input regions file to make sure chrom bin counter is accurate
@@ -14,31 +15,36 @@ current_chrom = ''
 region_counter = 0
 with open(regions_file_sorted, 'r') as in_f:
     in_regions = in_f.readlines()
-for chrom_counter, chrom_regions_file in enumerate(chrom_regions_files):
-    bin_counter = 1
-    with open(chrom_regions_file, 'w') as chrom_regions_f:
-        for line in in_regions[region_counter:]:
-            columns = line.split("\t")
-            chrom = columns[0]
-            if (chrom != current_chrom) and (current_chrom != ''): # not first chrom
-                current_chrom = chrom
-                break
-            elif chrom != current_chrom: # first chrom
-                current_chrom = chrom
-            region_counter += 1
-            region_start = int(columns[1])
-            region_end = int(columns[2])
-            bin_start = region_start
-            bin_end = bin_start + bin_size
-            while bin_start < region_end:
-                if bin_end > region_end:
-                    bin_end = region_end
-                bin_name = "{}{}".format(chrom_counter, bin_counter)
-                print("{}\t{}\t{}\t{}".format(chrom, bin_start, bin_end, bin_name), file=chrom_regions_f)
-                bin_counter += 1
-                # move
-                bin_start = bin_end
-                bin_end += bin_size
+with open(n_bins_file, 'w') as n_bins_f:
+    for chrom_counter, chrom_regions_file in enumerate(chrom_regions_files):
+        bin_counter = 1
+        with open(chrom_regions_file, 'w') as chrom_regions_f:
+            for line in in_regions[region_counter:]:
+                columns = line.split("\t")
+                chrom = columns[0]
+                if (chrom != current_chrom) and (current_chrom != ''): # not first chrom
+                    current_chrom = chrom
+                    break
+                elif chrom != current_chrom: # first chrom
+                    current_chrom = chrom
+                region_counter += 1
+                region_n_bins = 0
+                region_start = int(columns[1])
+                region_end = int(columns[2])
+                bin_start = region_start
+                bin_end = bin_start + bin_size
+                while bin_start < region_end:
+                    if bin_end > region_end:
+                        bin_end = region_end
+                    bin_name = "{}{}".format(chrom_counter, bin_counter)
+                    print("{}\t{}\t{}\t{}".format(chrom, bin_start, bin_end, bin_name), file=chrom_regions_f)
+                    bin_counter += 1 # counts the total number of bins for each chrom
+                    region_n_bins += 1 # counts the total number of bins for each region
+                    # move
+                    bin_start = bin_end
+                    bin_end += bin_size
+                print("{}\t{}\t{}\t{}".format(chrom, region_start, region_end, region_n_bins), file=n_bins_f)
+            
 ### combine regions from multiple chroms into a single file
 cmd = "cat"
 for f in chrom_regions_files:
--- workflow/scripts/run_annotation.py
+++ workflow/scripts/run_annotation.py
@@ -4,7 +4,9 @@ import ssm
 
 ### parameters
 in_files = snakemake.input.in_files
+n_bins_file = snakemake.input.n_bins_file
 model_json = snakemake.input.model_json
+features_dir = snakemake.params.features_dir
 feature_npz_files = snakemake.output.feature_npz_files
 
 ### model parameters
@@ -27,49 +29,68 @@ epigenomes = model_params["epigenomes"]
 assays = model_params["assays"]
 model_params = None
 
-### read input data
-if model_type == "stacked":
-    npz_objects = []
-    for in_f in in_files:
-        npz_objects.append(np.load(in_f))
-    X_df = np.vstack(([npz_obj["arr_0"] for npz_obj in npz_objects]))
-## concatenated model
-if model_type == "concatenated":
-    rows = []
-    for assay in assays:
-        col_npz_objects = []
-        for epigenome in epigenomes:
-            track = "{}_{}".format(epigenome, assay)
-            # find track input file
-            track_in_file = None
-            for in_f in in_files:
-                if in_f.split('/')[-1].startswith(track):
-                    track_in_file = in_f
-                    break
-            assert track_in_file != None
-            col_npz_objects.append(np.load(track_in_file))
-        rows.append(np.hstack(([npz_obj["arr_0"] for npz_obj in col_npz_objects])))
-    X_df = np.vstack(([row for row in rows]))
-##
-print("X_df: {}".format(X_df.shape)) # shape is E x G
-num_tracks = X_df.shape[0]
-num_positions = X_df.shape[1]
+### get the number of bins for each region
+n_bins_list = []
+with open(n_bins_file, 'r') as n_bins_f:
+    lines = n_bins_f.readlines()
+for line in lines:
+    n_bins_list.append(int(line.strip("\n").split("\t")[-1]))
 
-### data normalization
-X_df = np.arcsinh(X_df)
+bin_start = 0
+feature_mat = np.matrix([], dtype=np.single)
+for n_bins in n_bins_list:
+    bin_end = bin_start + n_bins
+    ### read input data
+    if model_type == "stacked":
+        npz_objects = []
+        for in_f in in_files:
+            npz_objects.append(np.load(in_f))
+        X_df = np.vstack(([npz_obj["arr_0"][bin_start:bin_end] for npz_obj in npz_objects]))
+    ## concatenated model
+    if model_type == "concatenated":
+        rows = []
+        for assay in assays:
+            col_npz_objects = []
+            for epigenome in epigenomes:
+                track = "{}_{}".format(epigenome, assay)
+                # find track input file
+                track_in_file = None
+                for in_f in in_files:
+                    if in_f.split('/')[-1].startswith(track):
+                        track_in_file = in_f
+                        break
+                assert track_in_file != None
+                col_npz_objects.append(np.load(track_in_file))
+            rows.append(np.hstack(([npz_obj["arr_0"][bin_start:bin_end] for npz_obj in col_npz_objects])))
+        X_df = np.vstack(([row for row in rows]))
+    ##
+    print("X_df: {}".format(X_df.shape)) # shape is E x n_bins
+    num_tracks = X_df.shape[0]
+    num_positions = X_df.shape[1]
 
-### run ssm annotation
-model = ssm.ssm(E=num_tracks, G=num_positions, K=K, \
-            lambda_1_l2=lambda_1_l2, lambda_2_l1=lambda_2_l1, lambda_2_l2=lambda_2_l2, lambda_3_l2=lambda_3_l2, \
-            positive_state=nonneg_state, sumone_state=sumone_state, positive_em=nonneg_em, message_passing=message_passing, \
-            verbose=False)
-model.set_x(np.asmatrix(X_df))
-model.set_theta(theta_m)
-model.set_lambda(lambda_m)
-model.update_state()
-print("y_m: {}".format(model.y_m.shape)) # shape is K x G
+    ### data normalization
+    X_df = np.arcsinh(X_df)
+
+    ### run ssm annotation
+    model = ssm.ssm(E=num_tracks, G=num_positions, K=K, \
+                lambda_1_l2=lambda_1_l2, lambda_2_l1=lambda_2_l1, lambda_2_l2=lambda_2_l2, lambda_3_l2=lambda_3_l2, \
+                positive_state=nonneg_state, sumone_state=sumone_state, positive_em=nonneg_em, message_passing=message_passing, \
+                verbose=False)
+    model.set_x(np.asmatrix(X_df))
+    model.set_theta(theta_m)
+    model.set_lambda(lambda_m)
+    model.update_state()
+    print("y_m: {}\n".format(model.y_m.shape)) # shape is K x n_bins
+
+    ### get the feature tracks
+    feature_mat = np.hstack([feature_mat, model.y_m]) if feature_mat.size else model.y_m
+
+    ### update bin index
+    bin_start = bin_end
+
+print("feature_mat: {}\n".format(feature_mat.shape)) # shape is K x G
 
 ### save the feature tracks
 ## npz
 for k in range(K):
-    np.savez_compressed(feature_npz_files[k], model.y_m[k, :]) # [:-4] to remove .npz from end
+    np.savez_compressed(feature_npz_files[k], feature_mat[k, :])
